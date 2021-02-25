6e55661553bf7263a18c5ccf4f9669d67408d8f6
aryan-green-apex <aryan.yadav@green-apex.com>
2021-02-25 17:35:26 +0530

    Updates

---
+++ precache-manifest.1d16d040dec401df83e8013c2c4d00a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3cab683d402bd2a050632a61f109878",
+    "url": "/contact/index.html"
+  },
+  {
+    "revision": "e729a9dc994a1e2858bc",
+    "url": "/contact/static/css/main.5c9eb0f2.chunk.css"
+  },
+  {
+    "revision": "fa22d279239c836e2237",
+    "url": "/contact/static/js/2.a402ca80.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/contact/static/js/2.a402ca80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e729a9dc994a1e2858bc",
+    "url": "/contact/static/js/main.96608f70.chunk.js"
+  },
+  {
+    "revision": "1caac261ed64c491d848",
+    "url": "/contact/static/js/runtime-main.48147e8d.js"
+  }
+]);
